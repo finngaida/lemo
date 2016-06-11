663aa4c9952ec47ec91cd18813d077aa534dd24f
Finn Gaida <f@fga.pw>
2016-06-11 21:20:05 +0200

    resize buttons correctly on touch down

--- iOS App/WeatherBot/BaseButton.swift
+++ iOS App/WeatherBot/BaseButton.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class BaseButton: UIButton {
     
-    var image:UIImageView?
-    var label:UILabel?
+    var image:UIImageView!
+    var label:UILabel!
     var origFrame = CGRectZero
+    var origImageFrame = CGRectZero
+    var origLabelFrame = CGRectZero
     let touchDownMargin:CGFloat = 10
     
     override init(frame: CGRect) {
@@ -28,20 +30,24 @@ class BaseButton: UIButton {
         image = UIImageView(frame: CGRectMake(touchDownMargin * 2, touchDownMargin, self.frame.width - touchDownMargin * 4, self.frame.width - touchDownMargin * 4))
         image?.layer.masksToBounds = true
         image?.layer.cornerRadius = image!.frame.width / 10
+        self.origImageFrame = image.frame
         self.addSubview(image!)
         
-        label = UILabel(frame: CGRectMake(0, self.frame.height * 5/6 - 20, self.frame.width, self.frame.height / 6))
+        label = UILabel(frame: CGRectMake(0, self.frame.height * 5/6 - 10, self.frame.width, self.frame.height / 6))
         label?.backgroundColor = .clearColor()
         label?.textColor = .whiteColor()
         label?.font = UIFont.systemFontOfSize(label!.frame.height / 1.5)
         label?.textAlignment = .Center
         label?.numberOfLines = 0
+        self.origLabelFrame = label.frame
         self.addSubview(label!)
     }
     
     func animate(small:Bool) {
         UIView.animateWithDuration(0.2, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0, options: [], animations: {
             self.frame = small ? CGRectMake(self.origFrame.origin.x + self.touchDownMargin, self.origFrame.origin.y + self.touchDownMargin, self.origFrame.width - self.touchDownMargin * 2, self.origFrame.height - self.touchDownMargin * 2) : self.origFrame
+            self.image?.frame = small ? CGRectMake(self.image!.frame.origin.x, self.image!.frame.origin.y - self.touchDownMargin, self.image!.frame.width - self.touchDownMargin * 2, self.image!.frame.height - self.touchDownMargin * 1.2) : self.origImageFrame
+            self.label?.frame = small ? CGRectMake(self.label!.frame.origin.x, self.label!.frame.origin.y - self.touchDownMargin, self.label!.frame.width - self.touchDownMargin * 2, self.label!.frame.height) : self.origLabelFrame
             }, completion: nil)
     }
     
